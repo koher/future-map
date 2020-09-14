99cadf26c624b815c35b6889bd1f5f5d94f04ce2
Yuta Koshizawa <koher@koherent.org>
2020-09-14 17:53:42 +0900

    Implement tests for `futureMap` and `futureFlatMap`

--- Tests/FutureMapTests/FutureMapTests.swift
+++ Tests/FutureMapTests/FutureMapTests.swift
@@ -15,4 +15,54 @@ final class FutureMapTests: XCTestCase {
         promise?(.success(3))
         cancellable.cancel()
     }
+    
+    func testFutureMap() {
+        do {
+            let expectation = XCTestExpectation()
+            
+            let a: Future<Int, Never> = .init { promise in
+                DispatchQueue.global().asyncAfter(deadline: .now() + 0.5) {
+                    promise(.success(3))
+                }
+            }
+            let r: Future<Int, Never> = a.futureMap { $0 * $0 }
+            
+            let cancellable = r.sink { value in
+                XCTAssertEqual(value, 9)
+                expectation.fulfill()
+            }
+            
+            wait(for: [expectation], timeout: 3.0)
+            
+            cancellable.cancel()
+        }
+    }
+    
+    func testFutureFlatMap() {
+        do {
+            let expectation = XCTestExpectation()
+            
+            let a: Future<Int, Never> = .init { promise in
+                DispatchQueue.global().asyncAfter(deadline: .now() + 0.5) {
+                    promise(.success(3))
+                }
+            }
+            let r: Future<Int, Never> = a.futureFlatMap { value in
+                Future { promise in
+                    DispatchQueue.global().asyncAfter(deadline: .now() + 0.5) {
+                        promise(.success(value * value))
+                    }
+                }
+            }
+            
+            let cancellable = r.sink { value in
+                XCTAssertEqual(value, 9)
+                expectation.fulfill()
+            }
+            
+            wait(for: [expectation], timeout: 3.0)
+            
+            cancellable.cancel()
+        }
+    }
 }
